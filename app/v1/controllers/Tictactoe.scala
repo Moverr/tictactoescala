4221c3e6076c0f396fc26628dc5714e7c00955a1
Muyinda Rogers <moverr@gmail.com>
2020-03-13 23:56:45 +0300

    Making a trait for scala  :

--- app/v1/controllers/Tictactoe.scala
+++ app/v1/controllers/Tictactoe.scala
@@ -2,9 +2,37 @@ package v1.controllers
 
 trait Tictactoe {
   //todo Array size of 3,3
-  def intiboard() : Array[String];
+  def intiBoard() : Array[String];
   def populateBoard() : Unit;
-  def findVertical(): Unit;
-  def findDiagonal():Unit;
-  def returnResult():Unit;
+  def findHorizontalMatch(): Unit;
+  def rotateThroughBoardColumns():Unit;
+  def findVerticalMatch():Unit;
+  def findifExistsUnmatched():Unit;
+  def findLeftRigtDiagonalMatch():Unit;
+  def findRightLeftDiagonalMatch():Unit;
+  def validateBoardString():Unit;
+  def placeHorizontalWin():Unit;
+  def playLeftRigtDiagonalWin():Unit;
+  def playRightLeftDiagonalWin():Unit;
+  def placeHorizontalBlock():Unit;
+  def placeVerticalBlock():Unit;
+
+  def placeLeftRigtDiagonalBlock():Unit;
+  def placeRightLeftDiagonalBlock():Unit;
+
+  def playHorizontalMove():Unit;
+  def playVerticalMove():Unit;
+  def playLeftRigtDiagonalMove():Unit;
+  def playRightLeftDiagonalMove():Unit;
+
+  // try to use a clojure
+  def shuffle():Unit;
+  def playNextMove():Unit;
+
+  def playGame():Unit;
+  def initGame():Unit;
+  def populateResponse():Unit;
+
+
+
 }
